c1e2318e98e7ff72fcf674c2eb5705ffc6c06ba6
Jesús Briceño <jesusbingucv@gmail.com>
2026-03-15 19:20:37 -0500

    [REF] Change Enum for StrEnum

--- settings.py
+++ settings.py
@@ -1,18 +1,25 @@
-from enum import Enum
+from enum import StrEnum, auto
+from typing import TypeVar, Type
+
+E = TypeVar('E', bound=StrEnum)
 
 
-class Options(Enum):
-    TRANS = "trans"
-    DELETE = "delete"
+class Options(StrEnum):
+    TRANS = auto()
+    DELETE = auto()
 
 
-class Formats(Enum):
+class Formats(StrEnum):
     MP3 = ".mp3"
     MP4 = ".mp4"
 
 
-ALLOWED_CLI_OPTIONS = [option.value for option in Options]
-ALLOWED_FORMATS = [format.value for format in Formats]
+def _get_enum_values(enum: Type[E]) -> list[str]:
+    return [member.value for member in enum]
+
+
+ALLOWED_CLI_OPTIONS = _get_enum_values(Options)
+ALLOWED_FORMATS = _get_enum_values(Formats)
 
 UPLOADS_FILE_FOLDER = "uploads"
 DOWNLOADS_FILE_FOLDER = "downloads"
